d3d5eabc7e55094b5417c6756e2ba213b6c2695d
Sebastiaan <sebastiaan@arendsen.dev>
2024-11-24 15:09:14 +0100

    fixup trpc setup

--- server/api/trpc/context.ts
+++
@@ -1,7 +0,0 @@
-import type { H3Event } from "h3";
-import { inferAsyncReturnType } from "@trpc/server";
-
-export const createContext = async (event: H3Event) => {
-  return { prisma: event.context.prisma };
-};
-export type Context = inferAsyncReturnType<typeof createContext>;
--- server/trpc/context.ts
+++ server/trpc/context.ts
@@ -1,5 +1,5 @@
-import type { inferAsyncReturnType } from '@trpc/server'
-import type { H3Event } from 'h3'
+import type { inferAsyncReturnType } from "@trpc/server";
+import type { H3Event } from "h3";
 
 /**
  * Creates context for an incoming request
@@ -12,7 +12,7 @@ export function createContext(_event: H3Event) {
    * return { prisma: _event.context.prisma }
    * ```
    */
-  return {}
+  return { prisma: _event.context.prisma };
 }
 
-export type Context = inferAsyncReturnType<typeof createContext>
+export type Context = inferAsyncReturnType<typeof createContext>;
