873d9e6f0f059c06a471e670a37e31299ef5e94b
Sebastiaan <sebastiaan@arendsen.dev>
2024-11-24 16:02:58 +0100

    cleanup router and remove example endpoint

--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -1,20 +1,7 @@
 import { z } from "zod";
-import { publicProcedure, router } from "../trpc";
+import { publicProcedure, protectedProcedure, router } from "../trpc";
 
-export const appRouter = router({
-  hello: publicProcedure
-    .input(
-      z.object({
-        text: z.string().nullish(),
-      }),
-    )
-    .query(({ input }) => {
-      return {
-        greeting: `hello ${input?.text ?? "world"}`,
-        time: new Date(),
-      };
-    }),
-});
+export const appRouter = router({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
