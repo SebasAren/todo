4226bc056aeb3e7450bf2320af98ea48e3e5526f
Sebastiaan <sebastiaan@arendsen.dev>
2024-11-24 16:31:53 +0100

    simple todo implementation in frontend

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <n-input v-model="newTodo" type="text" placeholder="New todo"></n-input>
+  <n-flex>
+    <n-input
+      v-model:value="newTodo"
+      type="text"
+      placeholder="New todo"
+      @keydown.enter="createTodo"
+    ></n-input>
+  </n-flex>
+  <ul>
+    <li v-for="todo in todos">{{ todo }}</li>
+  </ul>
 </template>
 
 <script setup lang="ts">
+const { $client } = useNuxtApp();
 const newTodo = ref("");
+const { todos, refreshTodos } = useTodos();
+const createTodo = () => {
+  $client.createTodo.mutate(newTodo.value);
+  refreshTodos();
+};
 </script>
