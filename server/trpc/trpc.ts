a6b3f1cde229ce158fe1509b160fa5a73f2ce330
Sebastiaan <sebastiaan@arendsen.dev>
2024-11-24 15:59:23 +0100

    added protected trpc endpoints

--- server/trpc/trpc.ts
+++ server/trpc/trpc.ts
@@ -7,17 +7,28 @@
  * @see https://trpc.io/docs/v10/router
  * @see https://trpc.io/docs/v10/procedures
  */
-import { initTRPC } from '@trpc/server'
-import superjson from 'superjson'
-import type { Context } from '~/server/trpc/context'
+import { initTRPC, TRPCError } from "@trpc/server";
+import superjson from "superjson";
+import type { Context } from "~/server/trpc/context";
 
 const t = initTRPC.context<Context>().create({
-  transformer: superjson
-})
+  transformer: superjson,
+});
 
 /**
  * Unprotected procedure
  */
-export const publicProcedure = t.procedure
-export const router = t.router
-export const middleware = t.middleware
+export const publicProcedure = t.procedure;
+export const router = t.router;
+export const middleware = t.middleware;
+
+// protected procedure
+export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
+  // to simplify the user models we'll just use email as a unique identifier
+  // this is a horrible idea for any actual applications.
+  if (ctx.session?.user?.email) {
+    return next({ ctx: { ...ctx, email: ctx.session.user.email } });
+  } else {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+});
