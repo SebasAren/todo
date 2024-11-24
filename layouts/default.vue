1b6cab0edd213f049a6d7004c163d7ffba065a62
Sebastiaan <sebastiaan@arendsen.dev>
2024-11-24 15:51:13 +0100

    fixed login system

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-  <n-page-header subtitle="An app to store your todos" @back="back">
+  <n-page-header
+    subtitle="An app to store your todos"
+    @back="back"
+    class="px-1"
+  >
     <n-grid :cols="2">
       <n-gi>
         <n-statistic label="To do" value="125" />
@@ -15,10 +19,17 @@
         src="https://upload.wikimedia.org/wikipedia/commons/6/67/Microsoft_To-Do_icon.png"
       />
     </template>
+    <template #extra
+      ><n-button v-if="status === 'unauthenticated'" @click="signIn('github')"
+        >Login</n-button
+      >
+      <n-button v-else @click="signOut()">Logout</n-button>
+    </template>
   </n-page-header>
   <slot />
 </template>
 
 <script setup lang="ts">
 const { back } = useRouter();
+const { signIn, signOut, status } = useAuth();
 </script>
