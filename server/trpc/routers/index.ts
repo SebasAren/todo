c549b7eeb0f5dc5dc0489680dc00f9e75f5324bb
Sebastiaan <sebastiaan@arendsen.dev>
2024-11-24 16:30:46 +0100

    added query to create and get your todos

--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -1,7 +1,24 @@
 import { z } from "zod";
-import { publicProcedure, protectedProcedure, router } from "../trpc";
+import { protectedProcedure, router } from "../trpc";
 
-export const appRouter = router({});
+export const appRouter = router({
+  getTodos: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.todo.findMany({
+      where: { userEmail: { equals: ctx.email } },
+    });
+  }),
+  createTodo: protectedProcedure
+    .input(z.string())
+    .mutation(({ ctx, input }) => {
+      console.log(input);
+      return ctx.prisma.todo.create({
+        data: {
+          userEmail: ctx.email,
+          description: input,
+        },
+      });
+    }),
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
